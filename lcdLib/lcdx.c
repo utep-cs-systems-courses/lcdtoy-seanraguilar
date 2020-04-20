cdb6cb030a8f2d05b7f81fbd1d0c89f8befa2ae2
seanraguilar <seanraguilar1999@gmail.com>
2020-04-20 11:05:53 -0600

    Drew My Own Trangle and Second Lab Triangle

--- lcdLib/lcdx.c
+++ lcdLib/lcdx.c
@@ -9,8 +9,9 @@ main()
   lcd_init();
   u_char width = screenWidth, height = screenHeight;
 
-  clearScreen(COLOR_BLUE);
+  clearScreen(COLOR_BLACK);
 
+  // First Triangle in Lab Coordanates (10, 10), (10, 25), (25, 25)
   int j;
   for (j = 10; j < 25; j++)
     drawPixel(j, j, COLOR_WHITE);
@@ -19,4 +20,27 @@ main()
   for (j = 10; j < 25; j++)
     drawPixel(10, j, COLOR_WHITE);
 
-}
+  for (j = 25; j < 40; j++) // Slope
+    drawPixel(j, j, COLOR_WHITE);
+  for (j = 25; j < 40; j++) // Horizontal
+    drawPixel(j, 40, COLOR_WHITE);
+  for (j = 25; j < 40; j++) // Vertical
+    drawPixel(25, j, COLOR_WHITE);
+
+  for (j = 25; j < 40; j++) // Slope 
+    drawPixel(j-15, j, COLOR_WHITE);
+  for (j = 25; j < 40; j++) // Vertical
+    drawPixel(10, j, COLOR_WHITE);
+  for (j = 10; j < 25; j++) // Horizontal
+    drawPixel(j, 40, COLOR_WHITE);
+  
+  clearScreen(COLOR_BLACK); // To Clear and Show...
+  
+  // Second Triangle in Lab
+  for (j = 0; j < 40; j++)
+    drawPixel(0, j, COLOR_RED);
+  for (j = 0; j < 40; j++)
+    drawPixel((j/2), 40, COLOR_RED);
+  for (j = 0; j < 40; j++)
+    drawPixel((j/2), j, COLOR_RED);
+  }
