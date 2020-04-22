6fe03260d8cdd1d1ceab4af5249c8d764b0dc7fd
seanraguilar <seanraguilar1999@gmail.com>
2020-04-22 10:03:00 -0600

    Lab Trapezoid

--- lcdLib/lcdx.c
+++ lcdLib/lcdx.c
@@ -43,4 +43,33 @@ main()
     drawPixel((j/2), 40, COLOR_RED);
   for (j = 0; j < 40; j++)
     drawPixel((j/2), j, COLOR_RED);
+
+  // Third Triangle in Lecture
+  int c, r;
+  for (c = 0; c <= 10; c++){
+    for (r = c; r<=10; r++) {
+      drawPixel(50+c, 50+r, COLOR_RED);
+      drawPixel(50+c, 50-r, COLOR_BLUE);
+      drawPixel(50-c, 50+r, COLOR_GREEN);
+      drawPixel(50-c, 50-r, COLOR_YELLOW);
+    }
   }
+
+  clearScreen(COLOR_BLACK); // To Clear and Show...
+
+  
+  // In Lab Trapezoid
+  for (c = 0; c <= 10; c++){
+    for (r = c; r<=10; r++){
+      drawPixel(20-c, 20+r, COLOR_WHITE); // Triangle
+      drawPixel(40+c, 20+r, COLOR_WHITE); // Triangle
+    }
+  }
+  //Rectangle
+  for (c = 0; c <= 20; c++){
+    for (r = 0; r <= 10; r++){
+      drawPixel(c+20,r+20,COLOR_WHITE); // Rectangle
+    }
+  }
+  
+}
