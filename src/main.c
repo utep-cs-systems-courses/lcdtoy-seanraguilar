4ba6a3ea32fd9526f7ceb9a984847fa9639b8907
seanraguilar <seanraguilar1999@gmail.com>
2020-05-02 19:58:46 -0600

    Added main.c

---
+++ src/main.c
@@ -0,0 +1,25 @@
+#include <msp430.h>
+#include "libTimer.h"
+#include "lcdutils.h"
+#include "lcddraw.h"
+#include "buzzer.h"
+#include "led.h"
+#include "switches.h"
+
+
+int main() {
+
+  configureClocks();
+  switch_init();
+  buzzer_init();
+  led_init();
+  enableWDTInterrupts();
+
+
+  lcd_init();
+  u_char width = screenWidth, height = screenHeight;
+  clearScreen(COLOR_BLUE);
+
+
+  or_sr(0x18);    // CPU off, GIE on
+}
