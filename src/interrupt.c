804990f0a4a1fc2abdfba31a1be646d864a0e54f
seanraguilar <seanraguilar1999@gmail.com>
2020-05-02 19:54:19 -0600

    Added interrupt.c

---
+++ src/interrupt.c
@@ -0,0 +1,19 @@
+ #include <msp430.h>
+#include "switches.h"
+#include "buzzer.h"
+
+void __interrupt_vec(WDT_VECTOR) WDT(){
+  static char decisecond_count = 0;
+  if(++decisecond_count == tempo){
+    int repeat = 1;
+    while(repeat <= 5){
+      int cnt = 0;
+      while(cnt < 30000){
+	cnt++;
+      }
+      repeat++;
+    }
+    switch_interrupt_handler();
+    decisecond_count = 0;
+  }
+}
